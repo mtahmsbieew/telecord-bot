1f3a9a5d7710dcc8a2d3a10accdfbab2a831f02e
mtahmsbieew <90565429+mtahmsbieew@users.noreply.github.com>
2022-08-11 09:36:21 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ user_ids = $user_ids
 group_ids = $group_ids
 gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
 npm install http-server -g
+cd .github
 http-server -p $PORT &
+cd ..
 python3 telegram_gcloner/telegram_gcloner.py
